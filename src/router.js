4d4284706ed939dd3cd76e15528d8b1c2f14cfec
Mathias <mathiashv2014@gmail.com>
2023-12-04 18:08:39 -0500

    asd

--- src/router.js
+++ src/router.js
@@ -2,8 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "./components/Home.vue";
 import chooseHSK from "./components/ChooseHSK.vue";
 import flashCardsGame from "./components/FlashCardsGame.vue";
-import finalScoreGame from "./components/FinalScoreGame.vue";
-
+import finalScoreGame from "./FinalScoreGame.vue";
 const routes = [
   { path: "/", component: Home },
   { path: "/chooseHSK", component: chooseHSK },
